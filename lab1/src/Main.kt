e18f68b20a4999ad58c304c928c73aa7cc582b62
Ersek Csongor <ersek.csongor@student.ms.sapientia.ro>
2024-09-30 08:58:26 +0300

    lab1

---
+++ lab1/src/Main.kt
@@ -0,0 +1,13 @@
+
+fun main() {
+    println("Hello World!")
+    val x = 5;
+    val y = 12;
+    println(sum(x,y))
+    listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
